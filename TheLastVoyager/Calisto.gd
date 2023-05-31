69f33ed88d34c1825885a0f9e754975e3a352050
Juan José Fernández <juanjoseferna@javerianacali.edu.co>
2023-05-31 18:38:10 -0500

    v1.2

--- TheLastVoyager/Calisto.gd
+++ TheLastVoyager/Calisto.gd
@@ -2,9 +2,9 @@ extends Position2D
 
 var Velocidad = Vector2()
 var puede_saltar
-export (float) var GRAVEDAD = 100
-export (float) var VEL_MOVIMIENTO = 25
-export (float) var VEL_SALTO = 25
+export (float) var GRAVEDAD = 2000
+export (float) var VEL_MOVIMIENTO = 5000
+export (float) var VEL_SALTO = 2100
 
 func _ready():
 	pass
@@ -15,6 +15,8 @@ func _physics_process(delta):
 	if(Input.is_action_pressed("tecla_a")):
 		Velocidad.x = -VEL_MOVIMIENTO
 		get_node("Cuerpo/CalistoSpr").flip_h = false
+		if(puede_saltar):
+			get_node("animacion_J1").play("j_corriendo")
 		
 		if(Input.is_action_just_pressed("tecla_w")):
 			get_node("animacion_J1").play("j_diagarr")
@@ -24,13 +26,15 @@ func _physics_process(delta):
 			
 		elif(!get_node("animacion_J1").is_playing()):
 			get_node("animacion_J1").play("j_corriendo")
-			
-		elif(Input.is_action_just_released("tecla_w") || Input.is_action_just_released("tecla_s")):
-			get_node("animacion_J1").stop()
+		
+		elif(Input.is_action_just_released("tecla_w") || Input.is_action_just_released("tecla_espacio")):
+			pass
 		
 	elif(Input.is_action_pressed("tecla_d")):
 		Velocidad.x = VEL_MOVIMIENTO
 		get_node("Cuerpo/CalistoSpr").flip_h = true
+		if(puede_saltar):
+			get_node("animacion_J1").play("j_corriendo")
 		
 		if(Input.is_action_just_pressed("tecla_w")):
 			get_node("animacion_J1").play("j_diagarr")
@@ -38,16 +42,13 @@ func _physics_process(delta):
 		elif(Input.is_action_just_pressed("tecla_s")):
 			get_node("animacion_J1").play("j_diagab")
 			
-		elif(Input.is_action_just_released("tecla_w") || Input.is_action_just_released("tecla_s")):
-			get_node("animacion_J1").stop()
-			
-		elif(!get_node("animacion_J1").is_playing()):
-			get_node("animacion_J1").play("j_corriendo")
+		elif(Input.is_action_just_released("tecla_w") || Input.is_action_just_released("tecla_espacio")):
+			pass
 	else:
 		Velocidad.x = 0
 		get_node("animacion_J1").play("j_idle")
 
-	if(Input.is_action_pressed("tecla_espacio")):
+	if(Input.is_action_pressed("tecla_espacio") && puede_saltar):
 		Velocidad.y = -VEL_SALTO
 		get_node("animacion_J1").play("j_salto")
 
@@ -59,7 +60,5 @@ func _physics_process(delta):
 	if(get_node("Cuerpo").get_slide_collision(get_node("Cuerpo").get_slide_count()-1) != null):
 		var obj_colisionado = get_node("Cuerpo").get_slide_collision(get_node("Cuerpo").get_slide_count()-1).collider
 		if(obj_colisionado.is_in_group("suelo")):
-			if(puede_saltar == false):
-				puede_saltar = true
-				get_node("animacion_J1").stop()
+			puede_saltar = true
 			
