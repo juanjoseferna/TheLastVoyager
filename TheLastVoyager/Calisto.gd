3148fdb187668cf5a58b770e3c312b39ce7eebf5
Juan José Fernández <juanjoseferna@javerianacali.edu.co>
2023-05-31 20:04:56 -0500

    v1.3.1

--- TheLastVoyager/Calisto.gd
+++ TheLastVoyager/Calisto.gd
@@ -2,9 +2,9 @@ extends Position2D
 
 var Velocidad = Vector2()
 var puede_saltar
-export (float) var GRAVEDAD = 2000
-export (float) var VEL_MOVIMIENTO = 5000
-export (float) var VEL_SALTO = 2100
+export (float) var GRAVEDAD = 6000
+export (float) var VEL_MOVIMIENTO = 4000
+export (float) var VEL_SALTO = 6000
 
 func _ready():
 	pass
@@ -17,36 +17,18 @@ func _physics_process(delta):
 		get_node("Cuerpo/CalistoSpr").flip_h = false
 		if(puede_saltar):
 			get_node("animacion_J1").play("j_corriendo")
-		
-		if(Input.is_action_just_pressed("tecla_w")):
-			get_node("animacion_J1").play("j_diagarr")
-			
-		elif(Input.is_action_just_pressed("tecla_s")):
-			get_node("animacion_J1").play("j_diagab")
-			
-		elif(!get_node("animacion_J1").is_playing()):
-			get_node("animacion_J1").play("j_corriendo")
-		
-		elif(Input.is_action_just_released("tecla_w") || Input.is_action_just_released("tecla_espacio")):
-			pass
-		
 	elif(Input.is_action_pressed("tecla_d")):
 		Velocidad.x = VEL_MOVIMIENTO
 		get_node("Cuerpo/CalistoSpr").flip_h = true
 		if(puede_saltar):
 			get_node("animacion_J1").play("j_corriendo")
-		
-		if(Input.is_action_just_pressed("tecla_w")):
-			get_node("animacion_J1").play("j_diagarr")
-			
-		elif(Input.is_action_just_pressed("tecla_s")):
-			get_node("animacion_J1").play("j_diagab")
-			
-		elif(Input.is_action_just_released("tecla_w") || Input.is_action_just_released("tecla_espacio")):
-			pass
 	else:
 		Velocidad.x = 0
-		get_node("animacion_J1").play("j_idle")
+		if(puede_saltar):
+			get_node("animacion_J1").play("j_idle")
+		if(Input.is_action_pressed("tecla_s")):
+			if(puede_saltar):
+				get_node("animacion_J1").play("j_agachado")
 
 	if(Input.is_action_pressed("tecla_espacio") && puede_saltar):
 		Velocidad.y = -VEL_SALTO
